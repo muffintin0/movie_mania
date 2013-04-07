7dcacc6138f066963b364fa7d77f176f52c1e66e
muffintin0 <hmchem@gmail.com>
2013-04-07 00:01:44 -0400

    setup project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movies::Application.config.secret_token = '09dde5159d0e509b4366bde513ae9ad15f8a2748427bdf3929132c8ede06dd768722314e0a420cbf9e54ed2d0a394829f4f5674758087f6db4956b5c0d86a9a6'
